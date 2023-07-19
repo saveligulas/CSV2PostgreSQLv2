2a71a485697426345c2bfd9c400595869d706abb
saveligulas <saveli.gulas@gmail.com>
2023-07-19 10:21:28 +0200

    Initial commit

---
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/Csv2PostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package gulas.saveli.CSV2PostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csv2PostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csv2PostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gulas/saveli/CSV2PostgreSQL/Csv2PostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package gulas.saveli.CSV2PostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csv2PostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
