0090c5a87bc30b2416723e320830ce516d2f83d5
saveligulas <saveli.gulas@gmail.com>
2023-07-21 11:56:52 +0200

    added basic entity code

--- src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicField.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicField.java
@@ -1,7 +1,19 @@
 package gulas.saveli.CSV2PostgreSQL.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class DynamicField {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
 }
