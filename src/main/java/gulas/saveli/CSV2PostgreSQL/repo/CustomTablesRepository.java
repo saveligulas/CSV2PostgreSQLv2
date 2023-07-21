f16e6900283f96dd929a543d6df0549c6d4d224f
saveligulas <saveli.gulas@gmail.com>
2023-07-21 11:54:42 +0200

    added repo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTablesRepository.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTablesRepository.java
@@ -1,6 +1,6 @@
 package gulas.saveli.CSV2PostgreSQL.repo;
 
-import gulas.saveli.CSV2PostgreSQL.custom.CustomTables;
+import gulas.saveli.CSV2PostgreSQL.model.CustomTables;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
