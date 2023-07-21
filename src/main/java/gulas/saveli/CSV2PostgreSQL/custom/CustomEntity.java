f290e124b8e6a5281697b24cdfac8ac7485f9af2
saveligulas <saveli.gulas@gmail.com>
2023-07-21 11:48:22 +0200

    added id and basic data annotations

--- src/main/java/gulas/saveli/CSV2PostgreSQL/custom/CustomEntity.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/custom/CustomEntity.java
@@ -1,4 +1,17 @@
 package gulas.saveli.CSV2PostgreSQL.custom;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class CustomEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
 }
