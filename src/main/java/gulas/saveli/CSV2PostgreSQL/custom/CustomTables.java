f3ff9f6743debe9762dd7f8bcd9f7e0445835f4e
saveligulas <saveli.gulas@gmail.com>
2023-07-21 11:53:15 +0200

    changed name and changed map

--- src/main/java/gulas/saveli/CSV2PostgreSQL/custom/CustomTables.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/custom/CustomTables.java
@@ -5,13 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.util.Map;
+
 @Entity
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
-public class CustomEntity {
+public class CustomTables {
     @Id
     @GeneratedValue
     private Long id;
+
+    private Map<String, DynamicFieldEntity>
 }
