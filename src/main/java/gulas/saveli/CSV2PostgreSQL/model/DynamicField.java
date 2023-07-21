5f126b7225d64337db92a17fea46cecdba3d3a8c
saveligulas <saveli.gulas@gmail.com>
2023-07-21 12:06:25 +0200

    recode

--- src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicField.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicField.java
@@ -1,8 +1,6 @@
 package gulas.saveli.CSV2PostgreSQL.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -10,10 +8,15 @@ import lombok.*;
 @Setter
 @ToString
 @RequiredArgsConstructor
+@Table(name= "main_table", schema= "converter")
 public class DynamicField {
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "custom_entity_id") // This specifies the column with the foreign key to the main_table
+    private CustomTable customTable;
 
 
 }
