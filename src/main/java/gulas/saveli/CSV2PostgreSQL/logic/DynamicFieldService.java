79580db9d888f17b48f5944337e87701d5cb553b
saveligulas <saveli.gulas@gmail.com>
2023-07-24 13:06:20 +0200

    added annotations and repo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicFieldService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicFieldService.java
@@ -1,4 +1,13 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.repo.DynamicFieldRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DynamicFieldService {
+    @Autowired
+    private final DynamicFieldRepository dynamicFieldRepository;
 }
