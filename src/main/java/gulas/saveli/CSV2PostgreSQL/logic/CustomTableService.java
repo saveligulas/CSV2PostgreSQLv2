d3d60fb003241d82faccc744d82cfceb510be58a
saveligulas <saveli.gulas@gmail.com>
2023-07-25 20:15:32 +0200

    finished method

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
@@ -1,13 +1,23 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.model.CustomTable;
 import gulas.saveli.CSV2PostgreSQL.repo.CustomTableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomTableService {
     @Autowired
     private final CustomTableRepository customTableRepository;
+
+    public void createCustomTable(List<String> fieldKeys) {
+        CustomTable customTable = new CustomTable();
+        customTable.setOrderedKeys(fieldKeys);
+    }
+
+    public
 }
