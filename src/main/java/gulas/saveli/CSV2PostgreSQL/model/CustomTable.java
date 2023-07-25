c7e47889b6af629ab66d6d9bfea03df8072fdb30
saveligulas <saveli.gulas@gmail.com>
2023-07-25 20:48:29 +0200

    added todo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/model/CustomTable.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/model/CustomTable.java
@@ -24,7 +24,7 @@ public class CustomTable {
     private List<String> orderedKeys = new ArrayList<>();
 
     @OneToMany(mappedBy = "customTable", cascade = CascadeType.ALL, orphanRemoval = true)
-    @MapKey(name = "fieldName")
+    @MapKey(name = "fieldName") //TODO create separate Key Entity class
     @OrderBy("id")
     @ToString.Exclude
     private Map<String, DynamicField> dynamicFields = new HashMap<>();
