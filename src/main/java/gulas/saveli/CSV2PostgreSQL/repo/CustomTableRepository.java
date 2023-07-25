eb7c97837a3a61edbfa914ab7ebe43347fbedb65
saveligulas <saveli.gulas@gmail.com>
2023-07-25 20:29:58 +0200

    added name search

--- src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTableRepository.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTableRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomTableRepository extends JpaRepository<CustomTable, Long> {
+    CustomTable findByName(String name);
 }
