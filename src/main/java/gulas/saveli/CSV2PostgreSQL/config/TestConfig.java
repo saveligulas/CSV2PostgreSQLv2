d889dd31e6751bd2689c12e6356eb4962db46182
saveligulas <saveli.gulas@gmail.com>
2023-07-25 20:09:12 +0200

    added lombok annotation

--- src/main/java/gulas/saveli/CSV2PostgreSQL/config/TestConfig.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/config/TestConfig.java
@@ -1,12 +1,14 @@
 package gulas.saveli.CSV2PostgreSQL.config;
 
 import gulas.saveli.CSV2PostgreSQL.repo.CustomTableRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class TestConfig {
     @Autowired
     private final CustomTableRepository customTableRepository;
