2653eaf17df1d83deb9d3f0a706e40d9867c125b
saveligulas <saveli.gulas@gmail.com>
2023-07-25 20:08:53 +0200

    added repo and cmdlr

--- src/main/java/gulas/saveli/CSV2PostgreSQL/config/TestConfig.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/config/TestConfig.java
@@ -1,7 +1,18 @@
 package gulas.saveli.CSV2PostgreSQL.config;
 
+import gulas.saveli.CSV2PostgreSQL.repo.CustomTableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TestConfig {
+    @Autowired
+    private final CustomTableRepository customTableRepository;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+
+    }
 }
