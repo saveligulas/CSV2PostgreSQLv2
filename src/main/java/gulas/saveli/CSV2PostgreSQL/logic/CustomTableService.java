98210a03afff53c77b65cbdb093126c9ed331221
saveligulas <saveli.gulas@gmail.com>
2023-07-25 19:48:08 +0200

    added repo to service

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
@@ -1,7 +1,13 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.repo.CustomTableRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomTableService {
+    @Autowired
+    private final CustomTableRepository customTableRepository;
 }
