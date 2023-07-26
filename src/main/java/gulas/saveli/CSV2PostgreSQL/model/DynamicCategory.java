d940eececa57d9af3b15371a6e927db6370d2d25
saveligulas <saveli.gulas@gmail.com>
2023-07-26 18:33:55 +0200

    added new entity

---
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicCategory.java
@@ -0,0 +1,12 @@
+package gulas.saveli.CSV2PostgreSQL.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class DynamicCategory {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
