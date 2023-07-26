352e2cb1ba42b1bf7246da9597e926d6cb311740
saveligulas <saveli.gulas@gmail.com>
2023-07-26 19:02:09 +0200

    added method and todo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicCategoryService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicCategoryService.java
@@ -1,13 +1,23 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.model.DynamicCategory;
+import gulas.saveli.CSV2PostgreSQL.repo.DynamicCategoryRepository;
 import gulas.saveli.CSV2PostgreSQL.repo.DynamicFieldRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class DynamicCategoryService {
     @Autowired
-    private final DynamicFieldRepository dynamicFieldRepository;
+    private final DynamicCategoryRepository dynamicCategoryRepository;
+
+    public void createDynamicCategory(String name) {
+        DynamicCategory dynamicCategory = new DynamicCategory();
+        dynamicCategory.setName(name);
+        dynamicCategoryRepository.save(dynamicCategory); //TODO not save if with same name exists
+    }
 }
