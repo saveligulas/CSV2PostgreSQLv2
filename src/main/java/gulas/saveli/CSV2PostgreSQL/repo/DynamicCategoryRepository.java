5a62d7d41aefd86f6eddb9ab1e3471239f597611
saveligulas <saveli.gulas@gmail.com>
2023-07-28 23:02:36 +0200

    added query

--- src/main/java/gulas/saveli/CSV2PostgreSQL/repo/DynamicCategoryRepository.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/repo/DynamicCategoryRepository.java
@@ -2,9 +2,12 @@ package gulas.saveli.CSV2PostgreSQL.repo;
 
 import gulas.saveli.CSV2PostgreSQL.model.DynamicCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DynamicCategoryRepository extends JpaRepository<DynamicCategory, Long> {
-    DynamicCategory findByName(String name);
+    @Query("SELECT d FROM DynamicCategory d WHERE d.name = :name")
+    DynamicCategory findByName(@Param("name") String name);
 }
