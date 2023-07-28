f509868b57a76d376260fe9471c7e55151446b1f
saveligulas <saveli.gulas@gmail.com>
2023-07-28 23:02:32 +0200

    added query

--- src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTableRepository.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/repo/CustomTableRepository.java
@@ -2,9 +2,12 @@ package gulas.saveli.CSV2PostgreSQL.repo;
 
 import gulas.saveli.CSV2PostgreSQL.model.CustomTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomTableRepository extends JpaRepository<CustomTable, Long> {
-    CustomTable findByName(String name);
+    @Query("SELECT c FROM CustomTable c WHERE c.name = :name")
+    CustomTable findByName(@Param("name") String name);
 }
