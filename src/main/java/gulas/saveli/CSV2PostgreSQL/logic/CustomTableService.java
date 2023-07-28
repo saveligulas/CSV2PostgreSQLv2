471746dc736fca0743f73f89b780e666584df6e0
saveligulas <saveli.gulas@gmail.com>
2023-07-28 22:44:28 +0200

    removed legacy code and added todo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/CustomTableService.java
@@ -23,30 +23,5 @@ public class CustomTableService {
     @Autowired
     private final DynamicCategoryRepository dynamicCategoryRepository;
 
-    public void createCustomTable(List<String> fieldKeys, String name) {
-        CustomTable customTable = new CustomTable();
-        customTable.setOrderedKeys(fieldKeys);
-        customTable.setName(name);
-        customTableRepository.save(customTable);
-    }
-
-    @Transactional
-    public void addDynamicFieldToTableTest(String name, Long fieldId, Long categoryId) {
-        DynamicCategory dynamicCategory = dynamicCategoryRepository.findById(categoryId)
-                .orElseThrow(() -> new RuntimeException("DynamicCategory could not be found"));
-        DynamicField dynamicField = dynamicFieldRepository.findById(fieldId)
-                .orElseThrow(() -> new RuntimeException("DynamicField could not be found"));
-        CustomTable customTable = customTableRepository.findByName(name);
-        if(customTable == null) {
-            throw new RuntimeException("CustomTable could not be found");
-        }
-
-        customTable.getDynamicFields().put(dynamicCategory, dynamicField);
-    }
-
-    public void testCreateAndSaveCustomTable(String name, DynamicCategory category, DynamicField field) {
-        CustomTable customTable = new CustomTable();
-        customTable.setName(name);
-
-    }
+    //TODO redo
 }
