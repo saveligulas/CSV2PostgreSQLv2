64709a47d2ab4ee3b7a96ac0301a563238d18ac8
saveligulas <saveli.gulas@gmail.com>
2023-07-28 23:02:50 +0200

    added correct schema

--- src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicCategory.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/model/DynamicCategory.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@Table(name = "dynamic_category")
+@Table(name = "dynamic_category", schema = "converter")
 public class DynamicCategory {
     @Id
     @GeneratedValue
