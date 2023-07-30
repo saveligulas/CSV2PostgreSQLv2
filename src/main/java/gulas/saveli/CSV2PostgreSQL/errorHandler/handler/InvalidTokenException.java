29a22ad89422cc8492c7d7bd6f17ec25c921d14c
saveligulas <saveli.gulas@gmail.com>
2023-07-30 20:48:16 +0200

    unversioned file

---
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/errorHandler/handler/InvalidTokenException.java
@@ -0,0 +1,10 @@
+package gulas.saveli.CSV2PostgreSQL.errorHandler.handler;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+    public InvalidTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
