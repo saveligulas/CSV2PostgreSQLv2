3c9025c4b17f676a34d7c599e7d35e2340a102b0
saveligulas <saveli.gulas@gmail.com>
2023-07-30 20:48:02 +0200

    finished method and added transactional

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicCategoryService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/DynamicCategoryService.java
@@ -1,12 +1,15 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.errorHandler.handler.ApiRequestException;
 import gulas.saveli.CSV2PostgreSQL.model.CustomTable;
 import gulas.saveli.CSV2PostgreSQL.model.DynamicCategory;
+import gulas.saveli.CSV2PostgreSQL.model.DynamicField;
 import gulas.saveli.CSV2PostgreSQL.repo.DynamicCategoryRepository;
 import gulas.saveli.CSV2PostgreSQL.repo.DynamicFieldRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +24,18 @@ public class DynamicCategoryService {
         category.setName(name);
         category.setCustomTable(table);
         dynamicCategoryRepository.save(category);
+    }
+
+    public void changeDynamicCategoryName(String name, Long id) {
+        DynamicCategory category = dynamicCategoryRepository.findById(id)
+                .orElseThrow(() -> new ApiRequestException("Dynamic category with id " + id + " not found"));
+        category.setName(name);
+    }
+
+    @Transactional
+    public void addDynamicField(Long id, DynamicField field) {
+        DynamicCategory category = dynamicCategoryRepository.findById(id)
+                .orElseThrow(() -> new ApiRequestException("Dynamic category with id " + id + " not found"));
+        category.addDynamicField(field);
     }
 }
