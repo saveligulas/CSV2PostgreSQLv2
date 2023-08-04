1322b87cf3aeb31eae925405c56153651dcc916c
saveligulas <saveli.gulas@gmail.com>
2023-08-04 21:52:36 +0200

    renamed and made service

---
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/CSVReaderService.java
@@ -0,0 +1,8 @@
+package gulas.saveli.CSV2PostgreSQL.reader;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CSVReaderService {
+    private final au.com.bytecode.opencsv.CSVReader
+}
