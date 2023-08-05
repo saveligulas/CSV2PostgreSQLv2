c93e71239faa63575f0d6d5a4ffdc9e65936b0cc
saveligulas <saveli.gulas@gmail.com>
2023-08-05 23:46:37 +0200

    added home mapping

--- src/main/java/gulas/saveli/CSV2PostgreSQL/controller/TXTController.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/controller/TXTController.java
@@ -1,4 +1,15 @@
 package gulas.saveli.CSV2PostgreSQL.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/txt")
 public class TXTController {
+    @GetMapping
+    public ModelAndView txtHomeView() {
+        return new ModelAndView();
+    }
 }
