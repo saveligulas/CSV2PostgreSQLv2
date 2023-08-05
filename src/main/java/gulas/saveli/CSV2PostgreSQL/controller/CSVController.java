1cf339040b3a7db44df725f15340cdf6265b0bee
saveligulas <saveli.gulas@gmail.com>
2023-08-05 23:45:15 +0200

    added mappings

--- src/main/java/gulas/saveli/CSV2PostgreSQL/controller/CSVController.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/controller/CSVController.java
@@ -1,4 +1,11 @@
 package gulas.saveli.CSV2PostgreSQL.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/csv")
 public class CSVController {
+    @GetMapping
 }
