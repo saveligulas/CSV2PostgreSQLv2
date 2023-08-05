9d21a9ca77769180228f3f924f92288f2c9809c9
saveligulas <saveli.gulas@gmail.com>
2023-08-05 23:46:53 +0200

    added home mapping

--- src/main/java/gulas/saveli/CSV2PostgreSQL/controller/CSVController.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/controller/CSVController.java
@@ -3,9 +3,13 @@ package gulas.saveli.CSV2PostgreSQL.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/csv")
 public class CSVController {
     @GetMapping
+    public ModelAndView csvHomeView() {
+        return new ModelAndView();
+    }
 }
