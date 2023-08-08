e84235097476a2f49a41d85b36f283c0f28d1d4b
saveligulas <saveli.gulas@gmail.com>
2023-08-08 21:24:28 +0200

    added readers

--- src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 @Service
 @RequiredArgsConstructor
 public class TXTReaderService {
@@ -17,6 +20,8 @@ public class TXTReaderService {
     @Autowired
     private final DynamicFieldService dynamicFieldService;
     public void readAndSaveTxtFile(String path) {
-
+        FileReader fileReader = new FileReader(path);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        try
     }
 }
