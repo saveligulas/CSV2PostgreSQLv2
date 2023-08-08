173ec74dcbea386d5df4053ea2e2c2ad78746385
saveligulas <saveli.gulas@gmail.com>
2023-08-08 20:50:31 +0200

    added all services

--- src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
@@ -1,9 +1,19 @@
 package gulas.saveli.CSV2PostgreSQL.reader;
 
+import gulas.saveli.CSV2PostgreSQL.logic.CustomTableService;
+import gulas.saveli.CSV2PostgreSQL.logic.DynamicCategoryService;
+import gulas.saveli.CSV2PostgreSQL.logic.DynamicFieldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TXTReaderService {
+    @Autowired
+    private final CustomTableService service;
+    @Autowired
+    private final DynamicCategoryService dynamicCategoryService;
+    @Autowired
+    private final DynamicFieldService dynamicFieldService;
     public void readAndSaveTxtFile(String path) {
 
     }
