d685da39f7c912b8db7eed1efbd7df06cc8e97d6
saveligulas <saveli.gulas@gmail.com>
2023-08-08 20:50:51 +0200

    added annotation

--- src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
@@ -3,10 +3,12 @@ package gulas.saveli.CSV2PostgreSQL.reader;
 import gulas.saveli.CSV2PostgreSQL.logic.CustomTableService;
 import gulas.saveli.CSV2PostgreSQL.logic.DynamicCategoryService;
 import gulas.saveli.CSV2PostgreSQL.logic.DynamicFieldService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TXTReaderService {
     @Autowired
     private final CustomTableService service;
