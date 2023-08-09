24056ca9237264859c178bf7a9b2076a5c2ccf42
saveligulas <saveli.gulas@gmail.com>
2023-08-09 23:55:20 +0200

    added annotation

--- src/main/java/gulas/saveli/CSV2PostgreSQL/reader/CSVReaderService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/CSVReaderService.java
@@ -1,9 +1,11 @@
 package gulas.saveli.CSV2PostgreSQL.reader;
 
 import au.com.bytecode.opencsv.CSVReader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CSVReaderService {
     public void readAndSaveCSVFile(String path) {
 
