31a89a4b0b116d48d9351fa688b5c7e787778352
saveligulas <saveli.gulas@gmail.com>
2023-08-11 19:53:47 +0200

    completed method

--- src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/reader/TXTReaderService.java
@@ -5,7 +5,9 @@ import gulas.saveli.CSV2PostgreSQL.logic.CustomTableService;
 import gulas.saveli.CSV2PostgreSQL.logic.DynamicCategoryService;
 import gulas.saveli.CSV2PostgreSQL.logic.DynamicFieldService;
 import gulas.saveli.CSV2PostgreSQL.model.CustomTable;
+import gulas.saveli.CSV2PostgreSQL.model.DynamicCategory;
 import gulas.saveli.CSV2PostgreSQL.repo.CustomTableRepository;
+import gulas.saveli.CSV2PostgreSQL.repo.DynamicCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,8 @@ public class TXTReaderService {
     private final DynamicFieldService dynamicFieldService;
     @Autowired
     private final CustomTableRepository customTableRepository;
+    @Autowired
+    private final DynamicCategoryRepository dynamicCategoryRepository;
 
     private String createSourcePath(String fileNameWithoutExtension) {
         return "src/main/resources/files/" + fileNameWithoutExtension + ".txt";
@@ -33,18 +37,21 @@ public class TXTReaderService {
     public void readAndSaveTxtFile(String fileNameWithoutExtension) throws FileNotFoundException {
         FileReader fileReader = new FileReader(createSourcePath(fileNameWithoutExtension));
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String categoryName = "TXT_Body";
         try {
             customTableService.createAndSaveCustomTable(fileNameWithoutExtension);
             CustomTable table = customTableRepository.findByName(fileNameWithoutExtension)
                             .orElseThrow(() -> new ApiRequestException("Could not find custom table " + fileNameWithoutExtension));
-            dynamicCategoryService.createAndSaveDynamicCategory("TXT_Body", table);
+            dynamicCategoryService.createAndSaveDynamicCategory(categoryName, table);
             String line;
             StringBuilder builder = new StringBuilder();
             while ((line = bufferedReader.readLine()) != null) {
                 builder.append(line).append("\n");
             }
+            DynamicCategory category = dynamicCategoryRepository.findByName(categoryName)
+                    .orElseThrow(() -> new ApiRequestException("Could not find dynamic category " + categoryName));
             String text = builder.toString();
-            dy
+            dynamicFieldService.createAndSaveDynamicField(text, category);
         } catch (IOException e) {
             e.printStackTrace();
         }
