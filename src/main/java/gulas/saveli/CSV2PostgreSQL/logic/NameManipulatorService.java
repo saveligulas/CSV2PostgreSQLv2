16b38c6d16d1fc4a37af0de8f6a3a857b710751e
saveligulas <saveli.gulas@gmail.com>
2023-08-25 11:23:58 +0200

    added repo

--- src/main/java/gulas/saveli/CSV2PostgreSQL/logic/NameManipulatorService.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/logic/NameManipulatorService.java
@@ -1,7 +1,13 @@
 package gulas.saveli.CSV2PostgreSQL.logic;
 
+import gulas.saveli.CSV2PostgreSQL.repo.DistinctNameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NameManipulatorService {
+    @Autowired
+    private final DistinctNameRepository nameRepository;
 }
