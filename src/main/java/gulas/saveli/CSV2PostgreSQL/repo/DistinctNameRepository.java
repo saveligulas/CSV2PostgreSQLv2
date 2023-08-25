95fff107940b256f72de1af0b743c6867d30f0b0
saveligulas <saveli.gulas@gmail.com>
2023-08-25 11:20:09 +0200

    added query

--- src/main/java/gulas/saveli/CSV2PostgreSQL/repo/DistinctNameRepository.java
+++ src/main/java/gulas/saveli/CSV2PostgreSQL/repo/DistinctNameRepository.java
@@ -1,9 +1,15 @@
 package gulas.saveli.CSV2PostgreSQL.repo;
 
 import gulas.saveli.CSV2PostgreSQL.model.DistinctName;
+import gulas.saveli.CSV2PostgreSQL.model.DynamicCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DistinctNameRepository extends JpaRepository<DistinctName, Long> {
+    @Query("SELECT d FROM DistinctName d WHERE d.modelType = ?1")
+    Optional<DistinctName> findByType(String typeString);
 }
